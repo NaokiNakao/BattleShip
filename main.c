3274862033a16d63469fe4e0ae11c23e0871c65f
Naoki Nakao <naokinakao@gmail.com>
2021-01-28 16:49:27 -0500

    Despliegue

---
+++ main.c
@@ -0,0 +1,67 @@
+/*
+   Juego de BattleShip programado en C.
+
+   Autor : Naoki Nakao
+   Fecha :
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.c>
+
+/* Directivas de preprocesador */
+
+/* Prototipos de función */
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
